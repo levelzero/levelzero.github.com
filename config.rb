57cce01c9648c0fedcf83d20906365e524e649da
maxlvl0 <levelzero@bk.ru>
2012-12-22 14:46:55 +0300

    Third trying-m

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/home/maxim/saibot/octopress/public/"
+http_images_path = "/home/maxim/saibot/octopress/public/images"
+http_fonts_path = "/home/maxim/saibot/octopress/public/fonts"
+css_dir = "public/home/maxim/saibot/octopress/public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
